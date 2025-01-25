ef895b8986a1f065f6338fd6c1e37c75ea5f0296
AlNomanCSE <abdullahalnomancse@gmail.com>
2025-01-25 12:18:34 +0600

    added starter files

---
+++ src/main/java/com/noman/nbSchool/NbSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.noman.nbSchool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/noman/nbSchool/NbSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noman.nbSchool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
