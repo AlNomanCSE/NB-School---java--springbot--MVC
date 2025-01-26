e0b3c8d7ab0b73d4ecc36bf3741703a19d1ebd82
AlNomanCSE <abdullahalnomancse@gmail.com>
2025-01-26 20:38:30 +0600

    Request with Path Params using - @PathVariable

--- src/main/java/com/noman/nbSchool/contoller/HolidaysController.java
+++ src/main/java/com/noman/nbSchool/contoller/HolidaysController.java
@@ -5,7 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 import java.util.Arrays;
 import java.util.List;
@@ -15,10 +16,8 @@ import java.util.stream.Collectors;
 @Controller
 public class HolidaysController {
 
-    @GetMapping("/holidays")
-    public String displayHolidays(@RequestParam(required = false) boolean festival, @RequestParam(required = false) boolean federal, Model model) {
-        model.addAttribute("festival", festival);
-        model.addAttribute("federal", federal);
+    @GetMapping("/holidays/{display}")
+    public String displayHolidays(@PathVariable String display, Model model) {
 
         List<Holiday> holidays = Arrays.asList(
                 // National Holidays
@@ -34,8 +33,14 @@ public class HolidaysController {
         );
         List<Holiday> federalHolidays = holidays.stream().filter(h -> h.getType() == Holiday.Type.FEDERAL).collect(Collectors.toList());
         List<Holiday> festivalHolidays = holidays.stream().filter(h -> h.getType() == Holiday.Type.FESTIVAL).collect(Collectors.toList());
-        model.addAttribute("festival", festival);
-        model.addAttribute("federal", federal);
+        if(display!=null && display.equals("all")){
+            model.addAttribute("festival", true);
+            model.addAttribute("federal", true);
+        } else if (display!=null && display.equals("federal")) {
+            model.addAttribute("federal", true);
+        }else if (display!=null && display.equals("festival")) {
+            model.addAttribute("festival", true);
+        }
         model.addAttribute("FESTIVAL", festivalHolidays);
         model.addAttribute("FEDERAL", federalHolidays);
 
