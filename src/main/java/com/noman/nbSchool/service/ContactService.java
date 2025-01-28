cd0cc6396c1355a2ff44623b0b55088e2efee896
AlNomanCSE <abdullahalnomancse@gmail.com>
2025-01-28 21:01:32 +0600

    Added Security with custome name and password with Authorization

--- src/main/java/com/noman/nbSchool/service/ContactService.java
+++ src/main/java/com/noman/nbSchool/service/ContactService.java
@@ -5,8 +5,11 @@ import com.noman.nbSchool.model.Contact;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+
+
 @Slf4j
 @Service
+
 public class ContactService {
 
     public boolean saveMessageDetails(Contact contact) {
