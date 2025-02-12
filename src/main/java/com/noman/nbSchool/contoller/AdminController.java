50c57321b867a96981ed9d5af36337ecad8ab242
AlNomanCSE <abdullahalnomancse@gmail.com>
2025-02-12 20:11:07 +0600

    admin dashboard mostly done

--- src/main/java/com/noman/nbSchool/contoller/AdminController.java
+++ src/main/java/com/noman/nbSchool/contoller/AdminController.java
@@ -1,7 +1,9 @@
 package com.noman.nbSchool.contoller;
 
+import com.noman.nbSchool.model.Courses;
 import com.noman.nbSchool.model.NbClass;
 import com.noman.nbSchool.model.Person;
+import com.noman.nbSchool.repository.CoursesRepository;
 import com.noman.nbSchool.repository.NbClassRepository;
 import com.noman.nbSchool.repository.PersonRepository;
 import jakarta.servlet.http.HttpServletRequest;
@@ -23,6 +25,7 @@ import java.util.Optional;
 public class AdminController {
     private final NbClassRepository nbClassRepository;
     private final PersonRepository personRepository;
+    private final CoursesRepository coursesRepository;
     @RequestMapping("/displayClasses")
     public ModelAndView displayClasses(Model model, HttpServletRequest req) {
         model.addAttribute("request", req);
@@ -88,12 +91,12 @@ public class AdminController {
         modelAndView.setViewName("redirect:/admin/displayStudents?classId="+nbClass.getClassId());
         return modelAndView;
     }
+
     @GetMapping("deleteStudent")
-    public ModelAndView deleteStudent(Model model, @RequestParam int personId, HttpSession session,HttpServletRequest req) {
+    public ModelAndView deleteStudent(@RequestParam int personId, HttpSession session) {
         NbClass nbClass = (NbClass)session.getAttribute("nbClass");
         Optional<Person> person = personRepository.findById(personId);
         person.get().setNbClass(null);
-
         nbClass.getPersons().remove(person.get());
         nbClassRepository.save(nbClass);
         session.setAttribute("nbClass",nbClass);
@@ -101,4 +104,72 @@ public class AdminController {
         return modelAndView;
     }
 
+    @GetMapping("/displayCourses")
+    public ModelAndView displayCourses(Model model,HttpServletRequest req) {
+        model.addAttribute("request", req);
+        List<Courses> courses = coursesRepository.findAll();
+        ModelAndView modelAndView = new ModelAndView("courses_secure.html");
+        modelAndView.addObject("courses",courses);
+        modelAndView.addObject("course", new Courses());
+        return modelAndView;
+    }
+
+    @PostMapping("/addNewCourse")
+    public ModelAndView addNewCourse(Model model, @ModelAttribute("course") Courses course,HttpServletRequest req) {
+        model.addAttribute("request", req);
+        ModelAndView modelAndView = new ModelAndView();
+        coursesRepository.save(course);
+        modelAndView.setViewName("redirect:/admin/displayCourses");
+        return modelAndView;
+    }
+
+    @GetMapping("/viewStudents")
+    public ModelAndView viewStudents(Model model, @RequestParam int id
+            ,HttpSession session,@RequestParam(required = false) String error,HttpServletRequest req) {
+        model.addAttribute("request", req);
+        String errorMessage = null;
+        ModelAndView modelAndView = new ModelAndView("course_students.html");
+        Optional<Courses> courses = coursesRepository.findById(id);
+        modelAndView.addObject("courses",courses.get());
+        modelAndView.addObject("person",new Person());
+        session.setAttribute("courses",courses.get());
+        if(error != null) {
+            errorMessage = "Invalid Email entered!!";
+            modelAndView.addObject("errorMessage", errorMessage);
+        }
+        return modelAndView;
+    }
+    @PostMapping("/addStudentToCourse")
+    public ModelAndView addStudentToCourse(Model model, @ModelAttribute("person") Person person,
+                                           HttpSession session,HttpServletRequest req) {
+        model.addAttribute("request", req);
+        ModelAndView modelAndView = new ModelAndView();
+        Courses courses = (Courses) session.getAttribute("courses");
+        Person personEntity = personRepository.readByEmail(person.getEmail());
+        if(personEntity==null || !(personEntity.getPersonId()>0)){
+            modelAndView.setViewName("redirect:/admin/viewStudents?id="+courses.getCourseId()
+                    +"&error=true");
+            return modelAndView;
+        }
+        personEntity.getCourses().add(courses);
+        courses.getPersons().add(personEntity);
+        personRepository.save(personEntity);
+        session.setAttribute("courses",courses);
+        modelAndView.setViewName("redirect:/admin/viewStudents?id="+courses.getCourseId());
+        return modelAndView;
+    }
+
+    @GetMapping("/deleteStudentFromCourse")
+    public ModelAndView deleteStudentFromCourse(Model model, @RequestParam int personId,
+                                                HttpSession session) {
+        Courses courses = (Courses) session.getAttribute("courses");
+        Optional<Person> person = personRepository.findById(personId);
+        person.get().getCourses().remove(courses);
+        courses.getPersons().remove(person);
+        personRepository.save(person.get());
+        session.setAttribute("courses",courses);
+        ModelAndView modelAndView = new
+                ModelAndView("redirect:/admin/viewStudents?id="+courses.getCourseId());
+        return modelAndView;
+    }
 }
---
+++ src/main/java/com/noman/nbSchool/repository/CoursesRepository.java
@@ -0,0 +1,9 @@
+package com.noman.nbSchool.repository;
+
+import com.noman.nbSchool.model.Courses;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CoursesRepository extends JpaRepository<Courses, Integer> {
+}
